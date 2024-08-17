759f3bdb9bf6c7f163893a56e04a9d7a0788c157
Lobzter <olowo15tomiloba@gmail.com>
2024-08-17 14:17:42 +0100

    finished the frontend code using thymeleaf and vanilla css

--- src/main/java/com/dev/fileUpload/Controller/FileProcessController.java
+++ src/main/java/com/dev/fileUpload/Controller/FileProcessController.java
@@ -1,12 +1,12 @@
 package com.dev.fileUpload.Controller;
 
 import com.dev.fileUpload.Service.TextProcessorService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -16,12 +16,18 @@ import java.io.IOException;
  * 8/17/24
  */
 
-@RestController
+@Controller
 @AllArgsConstructor
-@CrossOrigin
+
 public class FileProcessController {
 
     private final TextProcessorService textProcessorService;
+
+
+    @GetMapping("/")
+  public String fileUpload() {
+      return "index";
+  }
 
     @PostMapping("/process-file")
     public ResponseEntity<String> processFile(@RequestParam("file") MultipartFile file) throws IOException {
